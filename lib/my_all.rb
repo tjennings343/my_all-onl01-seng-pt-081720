4958529a95b5dcd27356fa6104a534835a1b83fe
Trey Jennings <tjennings343@gmail.com>
2020-08-27 16:40:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,8 +4,8 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    i = i + 1
     block_return_values << yeild (collection[i])
+    i = i + 1
   end
 
 end
